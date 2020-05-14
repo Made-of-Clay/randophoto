9d3caef4d0b12127697145844f737d4e8243594f
Adam <adam.m.leis@gmail.com>
2020-05-14 14:59:44 -0400

    develop more App layout & add test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import PhotoThumbnails from './PhotoThumbnails';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <PhotoThumbnails />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="app w-full max-w-screen-md mx-auto">
+            <header className="text-center app__header">
+                <h1 className="text-2xl">RandoPhoto Gallery</h1>
+                <h2 className="md:w-1/2 mx-auto">
+                    A React experiment by <a href="http://adamleis.com" className="app__link--mine">Adam Leis</a>
+                </h2>
+            </header>
+
+            <div className="flex flex-wrap mt-4">
+                <div className="bg-gray-400 p-3 w-full sm:w-1/2 mx-auto">Photo viewer</div>
+                <PhotoThumbnails className="w-full md:w-1/2 md:max-h-3/4-screen overflow-y-scroll border border-gray-400" count="5" />
+            </div>
+
+            <footer className="text-center fixed bottom-0 w-full bg-gray-300 left-0 bottom-0 border-t border-gray-500 py-1">
+                <small>
+                    Photo API by <a href="https://picsum.photos" className="app__link--picsum">Picsum API</a> &amp;
+                    photos by <a href="https://unsplash.com" className="app__link--unsplash">Unsplash.com</a>.
+                    All photo credit is attributed to the photographers via Unsplash.
+                </small>
+            </footer>
+        </main>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+    const getContainer = () => render(<App />).container;
+
+    test('displays gallery name', () => {
+        const container = getContainer();
+        const mainHeader = container.querySelector('.app__header');
+        expect(mainHeader).toHaveTextContent('RandoPhoto Gallery');
+    });
+
+    const testLink = selector => {
+        const container = getContainer();
+        const elem = container.querySelector(selector);
+        expect(elem).toBeInTheDocument();
+    };
+    test('displays my link', () => testLink('.app__link--mine'));
+    test('displays picsum link', () => testLink('.app__link--picsum'));
+    test('displays unsplash link', () => testLink('.app__link--unsplash'));
+
+    test('renders photo thumbnails', () => {
+        const container = getContainer();
+        const thumbsContainer = container.querySelector('.photoThumbnails');
+        expect(thumbsContainer).toBeInTheDocument();
+    });
 });
