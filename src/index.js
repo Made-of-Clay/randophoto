9f20d98985c563cb122208cc4a417548cee6d3a4
Adam <adam.m.leis@gmail.com>
2020-06-05 15:16:06 -0400

    add redux devtool code; rm old styles
    ridiculous method of adding tools

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux';
-import './index-orig.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
-// TODO setup redux
 ReactDOM.render(
   <Provider store={store}>
     <App />
