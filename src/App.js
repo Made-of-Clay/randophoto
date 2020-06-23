67405b9c81637a469d89af30a94fa4432cdaa3c0
Adam <adam.m.leis@gmail.com>
2020-06-23 13:20:43 -0400

    center app wrapper; add todos

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import PhotoViewer from './PhotoViewer';
 
 function App() {
     return (
-        <main className="app w-full max-w-screen-md mx-auto">
+        <div className="app mx-auto w-full max-w-screen-md text-indigo-900">
             <header className="text-center app__header">
-                <h1 className="text-2xl">RandoPhoto Gallery</h1>
+                <h1 className="text-2xl text-orange-500">
+                    RandoPhoto Gallery
+                </h1>
                 <h2 className="md:w-1/2 mx-auto">
                     A React experiment by <a href="http://adamleis.com" className="app__link--mine">Adam Leis</a>
                 </h2>
             </header>
 
-            <div className="flex flex-wrap mt-4">
-                <PhotoViewer className="bg-gray-400 p-3 w-full sm:w-1/2 mx-auto" />
-                <PhotoThumbnails className="w-full md:w-1/2 md:max-h-3/4-screen overflow-y-scroll border border-gray-400" count="5" />
-            </div>
+            <main className="flex flex-wrap mt-4">
+                {/* TODO make thumbnails bigger/cards w/ more info */}
+                {/* TODO make viewer an overlay viewer with larger/full image (& still some credits? small footer?) */}
+                <PhotoViewer className="p-3 w-full sm:w-1/2 mx-auto" />
+                <PhotoThumbnails className="w-full md:w-1/2 md:max-h-3/4-screen overflow-y-auto" count="5" />
+            </main>
 
             <footer className="text-center fixed bottom-0 w-full bg-gray-300 left-0 bottom-0 border-t border-gray-500 py-1">
                 <small>
@@ -24,7 +28,7 @@ function App() {
                     All photo credit is attributed to the photographers via Unsplash.
                 </small>
             </footer>
-        </main>
+        </div>
     );
 }
 
