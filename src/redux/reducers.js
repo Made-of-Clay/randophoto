f2759f7cded4fda6fd733d86e2441e287599b969
Adam <adam.m.leis@gmail.com>
2020-06-23 13:22:37 -0400

    rename redux index
    better debugging

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,14 +1,22 @@
 const initState = {
   photos: [],
   focusedPhoto: null,
+  photoLoading: false,
 };
 
 const rootReducer = (state = initState, action) => {
   switch (action.type) {
     case 'UPDATE_PHOTOS':
+      console.log('from redux', action.photos[0], action.photos[0].id, action.photos)
+      console.log({
+        ...state,
+        photos: action.photos,
+        focusedPhoto: action.photos[0].id,
+      });
       return {
         ...state,
         photos: action.photos,
+        focusedPhoto: action.photos[0].id,
       };
       case 'FOCUS_PHOTO':
         return {
