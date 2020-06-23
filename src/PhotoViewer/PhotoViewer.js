f94c3b8e7b7b3d751149ba3abdb03e8ba3308e00
Adam <adam.m.leis@gmail.com>
2020-06-23 13:23:33 -0400

    add default props & some notes

--- src/PhotoViewer/PhotoViewer.js
+++ src/PhotoViewer/PhotoViewer.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class PhotoViewer extends React.Component {
+    static defaultProps = {
+        className: ''
+    }
+
     get imgWidth() {
         return 500; // this will be dynamic eventually based on screensize/viewer size
     }
@@ -10,10 +14,17 @@ class PhotoViewer extends React.Component {
         const {height, width} = this.focusedPhoto;
         return Math.floor((this.imgWidth * height) / width);
     }
-    get focusedPhoto() {
+    get focusedPhoto() { // fast enough it doesn't matter that this re-runs
         return this.props.photos.find(photo => photo.id === this.props.focusedPhoto);
     }
 
+    /*
+    Hypothetical note: if imgs load too slowly, some tricky something could be set up to load the img the first time
+    and swap the img out for a new element. The swapped img would have opacity:0 (display:none would
+    produce the same problem as swapping src). Might also need position:absolute for hidden images to ignore their layout
+    effects. This would only be necessary if (1) loading was too slow from server issues or connectivity or (2) some loading
+    msg could not be successfully produced due to weirdness of rerender loops (img.onLoad setting state triggers src change)
+    */
     render() {
         let photo;
         if (this.focusedPhoto) {
@@ -24,7 +35,7 @@ class PhotoViewer extends React.Component {
                 alt={`ID ${id} by ${author}`}
             />;
         } else {
-            photo = <div></div>;
+            photo = <span />;
         }
 
         return (
