5ad9905d5372fc73a1a529412c316a86ac17f61a
Adam <adam.m.leis@gmail.com>
2020-06-25 20:30:01 -0400

    cleanup reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -7,16 +7,9 @@ const initState = {
 const rootReducer = (state = initState, action) => {
   switch (action.type) {
     case 'UPDATE_PHOTOS':
-      console.log('from redux', action.photos[0], action.photos[0].id, action.photos)
-      console.log({
-        ...state,
-        photos: action.photos,
-        focusedPhoto: action.photos[0].id,
-      });
       return {
         ...state,
         photos: action.photos,
-        focusedPhoto: action.photos[0].id,
       };
       case 'FOCUS_PHOTO':
         return {
